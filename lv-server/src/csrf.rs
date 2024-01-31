ce85023d4f187538a0a879040c561d784effca78
Aelto <Aelto@users.noreply.github.com>
2024-01-31 13:23:59 +0100

    add csrf checks

--- lv-server/src/csrf.rs
+++ lv-server/src/csrf.rs
@@ -7,8 +7,16 @@ pub fn csrf_protection(
   }
 }
 
+pub fn is_csrf_safe(req: &actix_web::HttpRequest) -> bool {
+  has_csrf_header(req.head().headers())
+}
+
+fn has_csrf_header(headers: &actix_web::http::header::HeaderMap) -> bool {
+  headers.contains_key("X-LVSERVER-REQ")
+}
+
 fn csrf_header_guard(route: actix_web::Route) -> actix_web::Route {
   route.guard(actix_web::guard::fn_guard(|c| {
-    c.head().headers().contains_key("X-LVSERVER-REQ")
+    has_csrf_header(c.head().headers())
   }))
 }
